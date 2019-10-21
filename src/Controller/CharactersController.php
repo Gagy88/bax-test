75ab190ace238c50ed52754f00f4440a20d667be
Dragan Rapić <dragan.rapic@typeqast.com>
2019-10-21 14:22:41 +0200

    [New] Show characters in location

--- src/Controller/CharactersController.php
+++ src/Controller/CharactersController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Controller\ApiController;
-use App\Model\Characters;
 use App\Services\RickAndMortyApi;
 use Swagger\Annotations as SWG;
 use Symfony\Component\HttpFoundation\Request;
@@ -22,18 +21,16 @@ class CharactersController extends ApiController
     }
 
     /**
-     * @Route("/dimension", methods={"GET"})
-     *
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-
-    /**
      * List all characters that exist (or are last seen) in a given dimension.
      *
      * @Route("/dimension", methods={"GET"})
      * @SWG\Response(
      *     response=200,
      *     description="all characters that exist (or are last seen) in a given dimension",
+     * )
+     * @SWG\Response(
+     *     response="400",
+     *     description="Bad request"
      * )
      * @SWG\Parameter(
      *     name="dimension",
@@ -45,12 +42,18 @@ class CharactersController extends ApiController
      *
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function fetchAllCharactersFromGivenDimension(Request $paramFetcher)
+    public function fetchAllCharactersFromGivenDimension(Request $request)
     {
         $charactersId = [];
-        $dimension = $paramFetcher->get('dimension');
 
-        $dimensions = $this->rickyAndMortyService->getDimension($dimension);
+        if ($request->get('dimension') == '') {
+            $this->setStatusCode(400);
+            return $this->respondWithErrors("request param is missing");
+        } else {
+            $dimension = $request->get('dimension');
+        }
+
+        $dimensions = $this->rickyAndMortyService->getDimensions($dimension);
 
         if (!empty($dimensions['results'])) {
             foreach ($dimensions['results'] as $dimension) {
@@ -59,6 +62,57 @@ class CharactersController extends ApiController
                 }
             }
 
+            $response = $this->rickyAndMortyService->getCharactersById($charactersId);
+        } else {
+            $response = [];
+            $this->setStatusCode(204);
+        }
+
+        return $this->respond($response);
+    }
+
+    /**
+     * List all characters that exist (or are last seen) at a given location.
+     *
+     * @Route("/location", methods={"GET"})
+     * @SWG\Response(
+     *     response=200,
+     *     description="all characters that exist (or are last seen) at a given location",
+     * )
+     * @SWG\Response(
+     *     response="400",
+     *     description="Bad request"
+     * )
+     * @SWG\Parameter(
+     *     name="location",
+     *     in="query",
+     *     type="string",
+     *     description="The field used to get character in location"
+     * )
+     * @SWG\Tag(name="characters")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function fetchAllCharactersFromGivenLocation(Request $request)
+    {
+        $charactersId = [];
+
+        if ($request->get('location') == '') {
+            $this->setStatusCode(400);
+            return $this->respondWithErrors("request param is missing");
+        } else {
+            $location = $request->get('location');
+        }
+
+        $locations = $this->rickyAndMortyService->getLocations($location);
+
+        if (!empty($locations['results'])) {
+            foreach ($locations['results'] as $location) {
+                foreach ($location['residents'] as $resident) {
+                    array_push($charactersId, substr($resident, strrpos($resident, "/") + 1, strlen($resident)));
+                }
+            }
+            
             $response = $this->rickyAndMortyService->getCharactersById($charactersId);
         } else {
             $response = [];
--- src/Services/RickAndMortyApi.php
+++ src/Services/RickAndMortyApi.php
@@ -4,11 +4,31 @@ namespace App\Services;
 
 class RickAndMortyApi extends RickAndMortyApiWrapper
 {
-    public function getDimension(string $dimension)
+    /**
+     *
+     * @param string $dimension
+     * @return array
+     */
+    public function getDimensions(string $dimensionName): array
     {
-        return $this->getByParams('location', ['dimension' => $dimension]);
+        return $this->getByParams('location', ['dimension' => $dimensionName]);
     }
 
+    /**
+     *
+     * @param string $locationName
+     * @return array
+     */
+    public function getLocations(string $locationName): array
+    {
+        return $this->getByParams('location', ['name' => $locationName]);
+    }
+
+    /**
+     *
+     * @param array $id
+     * @return array
+     */
     public function getCharactersById(array $id)
     {
         return $this->getById('character/' . implode(", ", $id));
